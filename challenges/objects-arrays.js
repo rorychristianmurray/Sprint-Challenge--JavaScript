993e910e9e47c4f23070baaa8279db67bcf885b1
Rory Murray <rory.christian.murray@gmail.com>
2019-05-10 11:47:35 -0400

    added first arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,28 +8,54 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyrannosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: 7000,
+  length: 12,
+  period: 'Late Cretaceous'
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'herbivorous',
+  weight: 2000,
+  length: 9,
+  period: 'Late Cretaceous'
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: 15,
+  length: 1.8,
+  period: 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight + 'kg');
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length + 'm');
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
-
+tyrannosaurus.roar = function() {
+  return 'RAWERSRARARWERSARARARRRR!'
+}
+console.log(tyrannosaurus.roar());
 
 // ==== Arrays ====
 
@@ -50,7 +76,26 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+
+let getUniversities = function(arr, field) {
+  for(let i = 0; i < arr.length; i++) {
+    universities.push(arr[i][field]);
+  }
+}
+
+getUniversities(graduates, "university");
+
+console.log(universities);
+
+
+let sortedUnis = universities.sort(function(uni1, uni2) {
+  if (uni1.toUpperCase() < uni2.toUpperCase()) {
+    return -1
+  }
+  return 1
+});
+
+console.log(sortedUnis);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
